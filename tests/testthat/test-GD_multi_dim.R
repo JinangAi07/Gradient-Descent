e545f0830bc7dee2363914c8f573797ffe426938
Jinang Ai <aijinang07@gmail.com>
2024-09-03 16:00:57 +0200

    commit8

--- tests/testthat/test-GD_multi_dim.R
+++ tests/testthat/test-GD_multi_dim.R
@@ -1,17 +1,19 @@
 test_that("GD_multi_dim produces reasonable loss value", {
   # Test input data
-  x1 <- c(137.97, 104.50, 100.00, 124.32, 79.20, 99.00, 124.00, 114.00)
-  x2 <- c(3, 2, 2, 3, 1, 2, 3, 2)
-  x <- cbind(x1, x2)
-  y <- c(145.00, 110.00, 93.00, 116.00, 65.32, 104.00, 118.00, 81.00)
+  insurance <- read.csv('insurance.csv')
+  x1 <- insurance$age
+  x2 <- insurance$bmi
+  x3 <- insurance$children
+  x <- cbind(x1, x2, x3)
+  y <- insurance$charges
 
   # Run GD_multi_dim
   gd_result <- GD_multi_dim(x, y, iter = 500, display_step = 100)
   final_loss_gd <- gd_result$final_loss
 
   # Compare with linear model (lm)
-  model_lm <- lm(y ~ x1 + x2)
-  predictions_lm <- predict(model_lm, newdata = data.frame(x1, x2))
+  model_lm <- lm(y ~ x1 + x2 + x3)
+  predictions_lm <- predict(model_lm, newdata = data.frame(x1, x2, x3))
   loss_lm <- mean((y - predictions_lm)^2) / 2
 
   # Compare losses
--- tests/testthat/test-GD_one_dim.R
+++ tests/testthat/test-GD_one_dim.R
@@ -1,7 +1,8 @@
 test_that("GD_one_dim produces reasonable loss value", {
   # Test input data
-  x <- c(137.97, 104.50, 100.00, 124.32, 79.20, 99.00, 124.00, 114.00)
-  y <- c(145.00, 110.00, 93.00, 116.00, 65.32, 104.00, 118.00, 81.00)
+  insurance <- read.csv('insurance.csv')
+  x <- insurance$age
+  y <- insurance$charges
 
   # Run GD_one_dim
   gd_result <- GD_one_dim(x, y, iter = 100, display_step = 100)
